66090e1470227f631e3607c9f32b4f4ccdd56b07
GoldenAntsStudio <128895930+GoldenAntsStudio@users.noreply.github.com>
2024-04-20 11:20:54 +0300

    Add files via upload

---
+++ M1L4/main.py
@@ -0,0 +1,31 @@
+import telebot 
+from config import token
+
+from logic import Pokemon
+
+bot = telebot.TeleBot(token) 
+
+@bot.message_handler(commands=['go'])
+def go(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
+    else:
+        bot.reply_to(message, "Ты уже создал себе покемона")
+
+@bot.message_handler(commands=['ban'])
+def go(message):
+    if message.reply_to_message: 
+        chat_id = message.chat.id 
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id)
+
+
+
+bot.infinity_polling(none_stop=True)
+
